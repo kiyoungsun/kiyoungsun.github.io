620f4f4e5ac1b23bab3fa6754ee61fb45885898c
kiyoungsun <sunki0623@gmail.com>
2015-12-16 06:23:24 +0900

    ctx_prob

--- metronome_my.js
+++ metronome_my.js
@@ -149,11 +149,11 @@ function init(){
   radius: 25,
   color: 'blue',
   draw: function() {
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, true);
-    ctx.closePath();
-    ctx.fillStyle = this.color;
-    ctx.fill();
+    canvasContext.beginPath();
+    canvasContext.arc(this.x, this.y, this.radius, 0, Math.PI*2, true);
+    canvasContext.closePath();
+    canvasContext.fillStyle = this.color;
+    canvasContext.fill();
   }
 };
     ball.draw();

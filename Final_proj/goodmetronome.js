d5ec7442d5e0d6ec2b6643a4b3e2ce548766ea24
kiyoungsun <sunki0623@gmail.com>
2015-12-16 05:42:08 +0900

    what_setTimeout

--- Final_proj/goodmetronome.js
+++ Final_proj/goodmetronome.js
@@ -3,14 +3,14 @@ var isPlaying = false;		// Are we currently playing?
 var startTime;				// The start time of the entire sequence.
 var current16thNote;		// What note is currently last scheduled?
 var tempo = 120.0;			// tempo (in beats per minute)
-var lookahead = 25.0;		// How frequently to call scheduling function 
+var lookahead = 10000.0;		// How frequently to call scheduling function 
 							//(in milliseconds)
 var scheduleAheadTime = 0.1;	// How far ahead to schedule audio (sec)
 							// This is calculated from lookahead, and overlaps 
 							// with next interval (in case the timer is late)
 var nextNoteTime = 0.0;		// when the next note is due.
 var noteResolution = 0;		// 0 == 16th, 1 == 8th, 2 == quarter note
-var noteLength = 0.001;		// length of "beep" (in seconds)
+var noteLength = 0.05;		// length of "beep" (in seconds)
 var intervalID = 0;			// setInterval identifier.
 
 var canvas,       			// the canvas element
